b674358c507672804060d7eb0c62b1c682b388bb
IstiakAhamedAshik <istiak1439@gmail.com>
2024-12-14 12:40:33 +0600

    Employe all Page Done

--- src/components/CustomersPayment/Employe/Customers.jsx
+++ src/components/CustomersPayment/Employe/Customers.jsx
@@ -1,9 +1,171 @@
-
+import TableLoadingSkeleton from "@/components/common/loadingSkeleton/TableLoadingSkeleton";
+import Pagination from "@/components/common/pagination/Pagination";
+import { AuthContext } from "@/context/AuthProvider";
+import useDebounced from "@/hooks/useDebounced";
+import NoDataFound from "@/shared/NoDataFound/NoDataFound";
+import { BASE_URL } from "@/utils/baseURL";
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useEffect, useState } from "react";
 
 const Customers = () => {
-  return (
-    <div>Customers</div>
-  )
-}
+  const [serialNumber, setSerialNumber] = useState();
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const { user } = useContext(AuthContext);
 
-export default Customers
+  // handle item search function....
+  const handleSearchValue = (value) => {
+    setSearchValue(value);
+    setLimit(10);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    const newSerialNumber = (page - 1) * limit;
+    setSerialNumber(newSerialNumber);
+  }, [page, limit]);
+
+  const searchText = useDebounced({ searchQuery: searchValue, delay: 500 });
+  useEffect(() => {
+    setSearchTerm(searchText);
+  }, [searchText]);
+
+  const customer_publisher_id = user?.customer_publisher_id
+    ? user?.customer_publisher_id
+    : user?._id;
+
+  //Fetch Customer Data
+  const {
+    data: allCustomers = [],
+    isLoading,
+    // refetch,
+  } = useQuery({
+    queryKey: [
+      `/api/v1/customer/self_customer?customer_publisher_id=${customer_publisher_id}&page=${page}&limit=${limit}&searchTerm=${searchTerm}`,
+    ],
+    queryFn: async () => {
+      try {
+        const res = await fetch(
+          `${BASE_URL}/customer/self_customer?customer_publisher_id=${customer_publisher_id}&page=${page}&limit=${limit}&searchTerm=${searchTerm}`,
+          {
+            credentials: "include",
+          }
+        );
+
+        if (!res.ok) {
+          const errorData = await res.text();
+          throw new Error(
+            `Error: ${res.status} ${res.statusText} - ${errorData}`
+          );
+        }
+
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.error("Fetch error:", error);
+        throw error;
+      }
+    },
+  });
+
+  console.log(allCustomers);
+
+  return (
+    <>
+      <div className="flex justify-between mt-6">
+        <div>
+          <h1 className="text-2xl">Customers</h1>
+        </div>
+
+        <div className="mt-3">
+          <input
+            type="text"
+            defaultValue={searchTerm}
+            onChange={(e) => handleSearchValue(e.target.value)}
+            placeholder="Search Customers..."
+            className="w-full sm:w-[350px] px-4 py-1.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
+          />
+        </div>
+      </div>
+      <>
+        {isLoading === true ? (
+          <TableLoadingSkeleton />
+        ) : (
+          <div>
+            <div className="rounded-lg border border-gray-200 mt-6">
+              {allCustomers?.data?.length > 0 ? (
+                <div className="overflow-x-auto rounded-t-lg">
+                  <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+                    <thead className="ltr:text-left rtl:text-right bg-[#fff9ee]">
+                      <tr className="divide-x divide-gray-300  font-semibold text-center text-gray-900">
+                        <td className="whitespace-nowrap p-4 ">SL No</td>
+                        <td className="whitespace-nowrap p-4 ">
+                          Customer Name
+                        </td>
+                        <td className="whitespace-nowrap p-4 ">
+                          Customer Phone
+                        </td>
+                        <td className="whitespace-nowrap p-4 ">
+                          Customer Address
+                        </td>
+                        <td className="whitespace-nowrap p-4 ">
+                          Customer Wallet
+                        </td>
+                        <td className="whitespace-nowrap p-4 ">
+                          Customer Status
+                        </td>
+                      </tr>
+                    </thead>
+
+                    <tbody className="divide-y divide-gray-200 text-center">
+                      {allCustomers?.data?.map((customer, i) => (
+                        <tr
+                          key={customer?._id}
+                          className={`divide-x divide-gray-200 ${
+                            i % 2 === 0 ? "bg-white" : "bg-tableRowBGColor"
+                          }`}
+                        >
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {serialNumber + i + 1}
+                          </td>
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {customer?.customer_name}
+                          </td>
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {customer?.customer_phone}
+                          </td>
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {customer?.customer_address}
+                          </td>
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {customer?.customer_wallet}
+                          </td>
+                          <td className="whitespace-nowrap py-1.5 font-medium text-gray-700">
+                            {customer?.customer_status}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <NoDataFound />
+              )}
+            </div>
+          </div>
+        )}
+        <Pagination
+          setPage={setPage}
+          setLimit={setLimit}
+          totalData={allCustomers?.data?.length}
+          page={page}
+          limit={limit}
+        />
+      </>
+    </>
+  );
+};
+
+export default Customers;
